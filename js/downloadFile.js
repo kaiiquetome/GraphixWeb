6687792353cf3db9a2254ea381d851f4d5df7cfb
kaiiquetome <80923176+kaiiquetome@users.noreply.github.com>
2024-11-08 17:07:24 +0000

    Deploying to gh-pages from  @ 2b2f922783fb3a92a589f41bace597148ec4eeaf 🚀

---
+++ js/downloadFile.js
@@ -0,0 +1,10 @@
+﻿window.downloadFileFromBytes = (fileName, fileType, fileBytes) => {
+    var blob = new Blob([fileBytes], { type: fileType });
+
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = fileName;
+
+    link.click();
+    window.URL.revokeObjectURL(link.href);
+};
