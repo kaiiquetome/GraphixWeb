d09ca5d0e7a966c545518b8df7bf8c287c21cc10
STOQUE\abner.lima <abner.lima@stoque.com.br>
2024-11-08 16:46:37 -0300

    atualização pagina order + removido obrigatóriedade dos campos

--- GraphixWeb/Models/Account.cs
+++ GraphixWeb/Models/Account.cs
@@ -4,22 +4,14 @@ namespace GraphixWeb.Models
 {
     public class Account : BaseModel
     {
-        [Required(ErrorMessage = "Razão Social é obrigatória")]
         public string CorporateName { get; set; }
 
-        [Required(ErrorMessage = "CNPJ é obrigatório")]
         public string Cnpj { get; set; }
 
-
-        [Required(ErrorMessage = "Inscrição Estadual é obrigatória")]
-        [StringLength(9, ErrorMessage = "A Inscrição Estadual não pode exceder 9 caracteres")]
         public string IE { get; set; }
 
-        [Required(ErrorMessage = "Telefone é obrigatório")]
         public string Phone { get; set; }
 
-        [Required(ErrorMessage = "Email é obrigatório")]
-        [EmailAddress(ErrorMessage = "Email inválido")]
         public string Email { get; set; }
     }
 }
