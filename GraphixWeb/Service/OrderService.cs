8f6192d0516432cd618da0a006708644e62bdbbe
STOQUE\abner.lima <abner.lima@stoque.com.br>
2025-01-21 12:18:17 -0300

    Atualização cashflow altera regra de apresentação do SALDO

--- GraphixWeb/Service/OrderService.cs
+++ GraphixWeb/Service/OrderService.cs
@@ -11,39 +11,47 @@ namespace GraphixWeb.Service
         private readonly string _baseMethod;
         private readonly IMapper _mapper;
         private readonly ApiClient _apiClient;
+
         public OrderService(IMapper mapper, ApiClient apiClient)
         {
             _baseMethod = "order";
             _mapper = mapper;
             _apiClient = apiClient;
         }
+
         public async Task<CreateOrderResponseDto> Create(Order order)
         {
             return await _apiClient.PostAsync<CreateOrderResponseDto>(_baseMethod, _mapper.Map<OrderDto>(order));
         }
+
         public async Task Delete(int id)
         {
             await _apiClient.DeleteAsync<bool>($"{_baseMethod}/{id}");
         }
+
         public async Task<Order> Get(int id)
         {
             return await _apiClient.GetAsync<Order>($"{_baseMethod}/{id}");
         }
+
         public async Task<List<Order>> Get()
         {
             return await _apiClient.GetAsync<List<Order>>($"{_baseMethod}");
         }
+
         public async Task Update(Order order)
         {
             await _apiClient.PutAsync<bool>(_baseMethod, _mapper.Map<OrderDto>(order));
         }
+
         public async Task<byte[]> Download(int id, string deadLine)
         {
             return await _apiClient.DownloadFileAsync<byte[]>($"{_baseMethod}/download?Id={id}&OrderDeadline={deadLine}");
         }
+
         public async Task<byte[]> Export(string startDate, string endDate)
         {
-            return await _apiClient.DownloadFileAsync<byte[]>($"{_baseMethod}/export?startDate={startDate}&endDate={endDate}");
+            return await _apiClient.DownloadFileAsync<byte[]>($"{_baseMethod}/export?StartDate={startDate}&EndDate={endDate}");
         }
     }
 }
